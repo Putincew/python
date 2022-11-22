96d0e402468834e21423ce4f7fe9e6fd822a04a3
Putincew <infrakras@gmail.com>
2022-11-22 22:44:32 +0400

    22_11_22

--- project/main9.py
+++ project/main9.py
@@ -530,27 +530,46 @@ import re
 # print(m[1])
 # print(m[2])
 # print(m[0])
-
-print("hell")
-
+# print(re.search(reg, s).group(1))
 
 
+# text = """
+# Самара
+# Москва
+# Тверь
+# Уфа
+# Казань
+# """
+#
+# count = 0
+#
+# def repl_find(m):
+#     global count
+#     count += 1
+#     return f'<option value={count}>{m.group(1)}</option>\n'
+#
+#
+# print(re.sub(r"\s*(\w+)\s*", repl_find, text))
 
 
+# s = "<p>Изображение <img src='bg.jpg'> - фон страницы</p>"
+# reg = r'<img\s+[^>]*src=([\'"])(.+)\1>'
+# print(re.findall(reg, s))
+
+# s = "<p>Изображение <img src='bg.jpg'> - фон страницы</p>"
+# reg = r'<img\s+[^>]*src=(?P<q>[\'"])(.+)(?P=q)>'
+# print(re.findall(reg, s))
 
 
+# s = 'Самолет прилетает 10/23/2023. Будем рады вас видеть после 10/24/2023.'
+# reg = r'(\d{2})/(\d{2})/(\d{4})'
+# print(re.findall(reg, s))
+# print(re.sub(reg, r'\2.\1.\3', s))
 
 
-
-
-
-
-
-
-
-
-
-
+s = 'google.com and google.ru and yandex.ru'
+reg = r'(([a-z0-9-]{2,}\.)+[a-z]{2,4})'
+print(re.sub(reg, r'http://\1', s))
 
 
 
