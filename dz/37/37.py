37397003856b2d4f97d0fdf497f83bb47828ef69
Putincew <infrakras@gmail.com>
2023-01-19 17:55:04 +0400

    37

---
+++ dz/37/37.py
@@ -0,0 +1,18 @@
+﻿class Power:
+    def __init__(self, step):
+        self.step = step
+
+
+    def __call__(self, fn):
+        def wrap(a, b):
+            return f'Результат ({a} + {b}) **{self.step}: {fn(a, b) **self.step}'
+
+        return wrap
+
+
+@Power(3)
+def funk(a, b):
+    return a + b
+
+
+print(funk(2, 2))
