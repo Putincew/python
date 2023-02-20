1eef6fd3ef76d6ac04119cafadb71d63e156c0cc
Putincew <infrakras@gmail.com>
2023-02-20 19:42:49 +0400

    dz45

---
+++ dz/45/model.py
@@ -0,0 +1,56 @@
+﻿import pickle
+import os
+
+class Film:
+    def __init__(self, title, ganr, director, year, duration, study, cast):
+        self.title = title
+        self.ganr = ganr
+        self.director = director
+        self.year = year
+        self.duration = duration
+        self.study = study
+        self.cast = cast
+
+    def __str__(self):
+        return f'{self.title} ({self.ganr})'
+
+
+class FilmModel:
+    def __init__(self):
+        self.db_name = 'db.txt'
+        self.films =self.load_data() # {}
+
+    def add_film(self, dict_film):
+        film = Film(*dict_film.values())
+        self.films[film.title] = film
+
+    def get_all_films(self):
+        return self.films.values()
+
+    def get_single_film(self, user_title):
+        film = self.films[user_title]
+        dict_film = {
+            'название': film.title,
+            'жанр': film.ganr,
+            'режиссер': film.director,
+            'год выпуска': film.year,
+            'продолжительность': film.duration,
+            'студия': film.study,
+            'актеры': film.cast
+        }
+
+        return dict_film
+
+    def remove_film(self, user_title):
+        return self.films.pop(user_title)
+
+    def load_data(self):
+        if os.path.exists(self.db_name):
+            with open(self.db_name, 'rb') as f:
+                return pickle.load(f)
+        else:
+            return dict()
+
+    def save_data(self):
+        with open(self.db_name, 'wb') as f:
+            pickle.dump(self.films, f)
---
+++ dz/45/view.py
@@ -0,0 +1,67 @@
+﻿def add_title(text):
+    def wraper(fn):
+        def wrap(*args, **kwargs):
+            print(text.center(50, '='))
+            res = fn(*args, **kwargs)
+            print('=' * 50)
+            return res
+        return wrap
+    return wraper
+
+class UserInterface:
+
+    @add_title(' Ввод пользовательских данных ')
+    def wait_user_answer(self):
+        print('Действия с фильмами:')
+        print('1 - добавление фильма'
+              '\n2 - каталог фильмов'
+              '\n3 - просмотр определенного фильма'
+              '\n4 - удаление фильма')
+        print('q - выход из программы')
+        user_answer = input('Выберите вариант действия: ')
+        return user_answer
+
+    @add_title('  добавление фильма ')
+    def add_user_film(self):
+        dict_film = {
+            'название': None,
+            'жанр': None,
+            'режиссер': None,
+            'год выпуска': None,
+            'продолжительность': None,
+            'студия': None,
+            'актеры': None
+        }
+        for key in dict_film:
+            dict_film[key] = input(f'введите {key} фильма: ')
+
+        return dict_film
+
+    @add_title('каталог фильмов: ')
+    def show_all_films(self, films):
+
+        for ind, film in  enumerate(films, 1):
+            print(f'{ind}. {film}')
+
+    @add_title('ввод названия фильма: ')
+    def get_user_film(self):
+        user_film = input('введите название фильма: ')
+        return user_film
+
+    @add_title('просмотр фильма: ')
+    def show_single_film(sejf, film):
+        for key in film:
+            print(f'{key} фильма - {film[key]}')
+
+    @add_title('сообщение об ошибке')
+    def show_incorrect_title_error(self, user_title):
+        print(f'фильм {user_title} не существует')
+
+    @add_title('удаление фильма')
+    def remove_single_film(self,film):
+        print(f'фильм {film} был удален')
+
+    @add_title('сообщение об ошибке')
+    def show_incorrect_answer_error(self, answer):
+        print(f'варианта {answer} не существует')
+
