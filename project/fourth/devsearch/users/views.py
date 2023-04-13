ab1d2a6a1c24e8dd3416a024387ebcd666fd2a0d
Putincew <infrakras@gmail.com>
2023-04-13 06:51:48 +0400

    1304

---
+++ project/fourth/devsearch/users/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from .models import Profile
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+def profiles(request):
+    prof = Profile.objects.all()
+    context = {'profiles': prof}
+    return render(request, 'users/index.html', context)
+
+
+def user_profile(request, pk):
+    prof = Profile.objects.get(id=pk)
+
+    top_skill = prof.skill_set.exclude(descriotions__exact='')
+    other_skill = prof.skill_set.filter(descriotions='')
+
+    context = {
+        'profile': prof,
+        'top_skill': top_skill,
+        'other_skill': other_skill,
+    }
+    return render(request, 'users/profile.html', context)
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            print('Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            print('Не корректное имя пользователя или пароль')
+
+    return render(request, 'users/login_register.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
